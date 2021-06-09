c3c66beb2f378169c346009b2a4eeea57cc930e7
shivam pandey <shivam.pandey@thinkanalytics.in>
2021-06-09 20:59:00 +0530

    fdsadf

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import Message from "./Message";
 import Greeting from "./Greeting";
-import NumSign from "./NumSign";
-import Button from "@material/react-button";
 import "./index.css";
 import "@material/react-button/dist/button.css";
 
 function SuperLemon(props) {
-  const [number, setNumber] = useState(0);
-  const [squaredNum, setSquaredNum] = useState(0);
-  useEffect(() => {
-    setSquaredNum(number * number);
-  }, [number]);
   return (
     <div className="nonsense">
       <Greeting name={props.name} />
